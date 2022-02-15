81e4bdac7890510f0424e96db0af18f99df1474e
David Freifeld <freifeld.david@gmail.com>
2022-02-15 11:03:04 -0800

    Add some tests and basic model logic

--- nescient/__init__.py
+++ nescient/__init__.py
@@ -2,15 +2,16 @@ __version__ = "0.1.0"
 
 import numpy as np
 import torch
+from concrete.torch.compile import compile_torch_model
 from torch import nn
 
 
 class ConvNet(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 5, (5, 5))
-        self.linear = nn.Linear(5, 50)
-        self.out = nn.Linear(50, 2)
+        self.conv1 = nn.Conv2d(1, 1, (8, 8), stride=8)
+        self.linear = nn.Linear(32 * 32, 512)
+        self.out = nn.Linear(512, 2)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -18,5 +19,17 @@ class ConvNet(torch.nn.Module):
         return self.out(x)
 
 
-net = ConvNet()
-print(net)
+class ConvNetWrapper:
+    """I am bad at naming things."""
+
+    def __init__(self):
+        self.module = compile_torch_model(
+            ConvNet(),
+            torch.rand(1, 1, 256, 256),
+            n_bits=7,
+        )
+
+    def infer(self):
+        enc_x = np.array([np.random.randn(1, 1, 256, 256)]).astype(np.uint8)
+        out = self.module.forward_fhe.run(enc_x)
+        return self.module.dequantize_output(np.array(out, dtype=np.float32))
--- tests/test_nescient.py
+++ tests/test_nescient.py
@@ -1,5 +1,17 @@
-from nescient import __version__
+import nescient
+import torch
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert nescient.__version__ == "0.1.0"
+
+
+def test_convnet():
+    net = nescient.ConvNet()
+    x = torch.rand(1, 1, 256, 256)
+    net.forward(x)
+
+
+def test_wrapper():
+    net = nescient.ConvNetWrapper()
+    infer()
