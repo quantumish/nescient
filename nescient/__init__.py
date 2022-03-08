4cea94dc2617817d0bae6edc09c5f87e8a157400
David Freifeld <freifeld.david@gmail.com>
2022-03-08 11:16:19 -0800

    Sanity checks for model

--- nescient/__init__.py
+++ nescient/__init__.py
@@ -10,23 +10,25 @@ import crypten
 
 
 class ConvNet(torch.nn.Module):
-    """Simple conv net for binary classification of images."""
+    """Simple conv net for classification of chest X rays."""
     def __init__(self):
         """Initializes the model"""        
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 1, (8, 8), stride=8)
-        self.linear = nn.Linear(32, 16)
-        self.out = nn.Linear(16, 2)
+        self.conv1 = nn.Conv2d(1, 1, (8, 8), stride=4)
+        self.conv2 = nn.Conv2d(1, 1, (4, 4), stride=2)
+        self.linear = nn.Linear(3844, 120)
+        self.out = nn.Linear(120, 11)
 
     def forward(self, x):
         """
         Forward propagates the model.
         
         Arguments:
-        - x: a (1,1,256,256) black and white image.
+        - x: a (1, 1, 512, 512) grayscale image.
         """
         x = self.conv1(x)
-        x = self.linear(x)
+        x = self.conv2(x)    
+        x = self.linear(torch.flatten(x, 1))
         return self.out(x)
 
 
@@ -39,14 +41,14 @@ class ConvNetWrapper:
         """
         self.graph = crypten.nn.from_pytorch(
             model,
-            torch.rand(1, 1, 256, 256),
+            torch.rand(1, 1, 512, 512),
         ).encrypt()
 
     def encrypted_infer(self, x):
         """
         Peforms encrypted inference on an input.
         Argument: 
-        - x: a (1,1,256,256) black and white image. 
+        - x: a (1, 1, 512, 512) black and white image. 
         """
         self.graph.forward(x)
 
--- tests/test_nescient.py
+++ tests/test_nescient.py
@@ -8,13 +8,35 @@ import doctest
 def test_convnet():
     """Test if a ConvNet can be instantiated and forward propagated."""
     net = nescient.ConvNet()
-    x = torch.rand(1,1,256,256)
+    x = torch.rand(1,1,512,512)
     net.forward(x)
 
+def test_train_sanity():
+    """Model sanity check: make sure the model is able to train at all."""
+    net = nescient.ConvNet()
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
+    i = torch.rand(1,1,512,512)
+    label = torch.tensor([[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]])
+    outputs = net(i)
+    orig_loss = criterion(outputs, label)
+    for epoch in range(100):
+        optimizer.zero_grad()
+        outputs = net(i)
+        loss = criterion(outputs, label)
+        loss.backward()
+        optimizer.step()
+    assert(loss.item() < orig_loss)
+    return loss
+
+def test_train_well_sanity():
+    """Model sanity check: make sure the model can train to near zero loss on a single datapoint."""
+    loss = test_train_sanity()
+    assert(loss.item() < 0.01)
     
 def test_wrapper():
     """Test if a ConvNetWrapper can be instantiated and forward propagated."""
-    x = torch.rand(1,1,256,256)
+    x = torch.rand(1,1,512,512)
     x = crypten.cryptensor(x)
     net = nescient.ConvNetWrapper()
     net.encrypted_infer(x)
