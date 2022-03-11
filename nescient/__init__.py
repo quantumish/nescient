e856bb28f4a17f4e0c0e26b00a319e542315b907
David Freifeld <freifeld.david@gmail.com>
2022-03-10 22:04:33 -0800

    Data loading boilerplate

--- nescient/__init__.py
+++ nescient/__init__.py
@@ -1,57 +1,81 @@
-"""
-Library for encrypted classification of images.
-"""
+"""Library for encrypted classification of images."""
 __version__ = "0.1.0"
 
-import numpy as np
-import torch
-from torch import nn
 import crypten
+import numpy
+import torch
 from PIL import Image
+from torch import nn
 from torchvision.transforms import ToTensor
 
-class DataIterator():
+
+class DataIterator:
     def __init__(self, csv_path: str, data_folder: str):
-        self.file = open(path ,"r")
-    
+        self.data_folder = data_folder
+        self.file = open(csv_path, "r")
+
     def __iter__(self):
-        next(self.file) # skip header line
+        next(self.file)  # skip header line
         return self
 
     def __next__(self):
         line = next(self.file).split(",")
-        image = ToTensor()(Image.open(self.data_folder+'/'+line[0]))
-        label = torch.tensor([[i for i in line[6:]]])
-        return (image, label)
+        raw_image = Image.open(self.data_folder + "/" + line[0])
+        # print(raw_image.size, line[0])
+        if raw_image.size != (320, 390):
+            # print("Skipping!")
+            return None        
+        image = torch.tensor(
+            [numpy.array(
+                (Image.open(self.data_folder + "/" + line[0])).getdata(),
+                dtype=numpy.float32
+            ).reshape(1, 320, 390)]
+        )
+
+        def sketchy_float(i):
+            try:
+                return float(i)
+            except:
+                return 0.0
+
             
+        label = torch.tensor([[0 if i == '' else sketchy_float(i) for i in line[5:]]])
+        return (image, label)
+
+
 class ConvNet(torch.nn.Module):
     """Simple conv net for classification of chest X rays."""
+
     def __init__(self):
-        """Initializes the model"""        
+        """Initialize the model."""
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 1, (8, 8), stride=4)
+        self.conv1 = nn.Conv2d(1, 1, (32, 32), stride=4)
         self.conv2 = nn.Conv2d(1, 1, (4, 4), stride=2)
-        self.linear = nn.Linear(3844, 120)
-        self.out = nn.Linear(120, 11)
+        self.linear = nn.Linear(1786, 120)
+        self.out = nn.Linear(120, 14)
 
     def forward(self, x):
         """
         Forward propagates the model.
-        
+
         Arguments:
         - x: a (1, 1, 512, 512) grayscale image.
         """
         x = self.conv1(x)
-        x = self.conv2(x)    
+        x = self.conv2(x)
         x = self.linear(torch.flatten(x, 1))
         return self.out(x)
 
 
 class ConvNetWrapper:
-    """Wraps a ConvNet() into an encrypted model"""
+    """Wrap a ConvNet() into an encrypted model."""
+
     def __init__(self, model=ConvNet()):
-        """
-        Arguments: 
+        """Initialize the ConvNetWrapper.
+
+        Uses either the default constructor of ConvNet or an existing ConvNet.
+
+        Arguments:
         - model: a ConvNet
         """
         self.graph = crypten.nn.from_pytorch(
@@ -60,13 +84,12 @@ class ConvNetWrapper:
         ).encrypt()
 
     def encrypted_infer(self, x):
-        """
-        Peforms encrypted inference on an input.
-        Argument: 
-        - x: a (1, 1, 512, 512) black and white image. 
+        """Peforms encrypted inference on an input.
+
+        Argument:
+        - x: a (1, 1, 512, 512) black and white image.
         """
         self.graph.forward(x)
 
     # def train(self, inputs, labels):
     #     pass
-    
--- tests/test_nescient.py
+++ tests/test_nescient.py
@@ -1,22 +1,26 @@
 """Tests standard functionality of nescient."""
 
+import doctest
+from itertools import islice
+
+import crypten
 import nescient
 import torch
-import crypten
-import doctest
+
 
 def test_convnet():
     """Test if a ConvNet can be instantiated and forward propagated."""
     net = nescient.ConvNet()
-    x = torch.rand(1,1,512,512)
+    x = torch.rand(1, 1, 512, 512)
     net.forward(x)
+
 
 def test_train_sanity():
     """Model sanity check: make sure the model is able to train at all."""
     net = nescient.ConvNet()
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
-    i = torch.rand(1,1,512,512)
+    i = torch.rand(1, 1, 512, 512)
     label = torch.tensor([[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]])
     outputs = net(i)
     orig_loss = criterion(outputs, label)
@@ -26,30 +30,39 @@ def test_train_sanity():
         loss = criterion(outputs, label)
         loss.backward()
         optimizer.step()
-    assert(loss.item() < orig_loss)
+    assert loss.item() < orig_loss
     return loss
 
+
 def test_train_well_sanity():
-    """Model sanity check: make sure the model can train to near zero loss on a single datapoint."""
+    """Test if model can train to near zero loss on a single datapoint."""
     loss = test_train_sanity()
-    assert(loss.item() < 0.01)
+    assert loss.item() < 0.01
+
 
 def test_train_epoch():
     net = nescient.ConvNet()
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
-    it = nescient.DataIterator("./tests/data.csv", "~/aux")    
-    for i in it:
-        optimizer.zero_grad()
-        outputs = net(i[0])
-        loss = criterion(outputs, i[1])
-        loss.backward()
-        optimizer.step()
-    
-    
+    for n in range(100): 
+        it = nescient.DataIterator(
+             "/home/quantumish/aux/CheXpert-v1.0-small/train.csv", "/home/quantumish/aux"
+        )
+        for i in islice(it, 100):
+            if (i == None):
+                continue
+            optimizer.zero_grad()
+            outputs = net(i[0])
+            loss = criterion(outputs, i[1])
+            loss.backward()
+            optimizer.step()
+        print("Epoch {}/100: {}".format(n, loss.item()))
+    print(loss.item())
+
+
 def test_wrapper():
     """Test if a ConvNetWrapper can be instantiated and forward propagated."""
-    x = torch.rand(1,1,512,512)
+    x = torch.rand(1, 1, 512, 512)
     x = crypten.cryptensor(x)
     net = nescient.ConvNetWrapper()
     net.encrypted_infer(x)
